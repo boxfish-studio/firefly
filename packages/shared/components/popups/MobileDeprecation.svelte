c36eb8b9713f24f24988fdd5c231331f3a511bca
Begoña Alvarez <balvarez@boxfish.studio>
2023-07-05 12:29:44 +0200

    feat: add mobile deprecation popup

---
+++ packages/shared/components/popups/MobileDeprecation.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+    import { Text } from 'shared/components'
+</script>
+
+<div class="flex flex-col space-y-5 text-center">
+    <Text type="h4" classes="text-center">This is a title</Text>
+    <div class="flex flex-auto flex-grow justify-center items-center">
+        <Text type="p">This is a body text</Text>
+    </div>
+</div>
+
+<style lang="scss">
+    div {
+        min-height: 80vh;
+    }
+</style>
